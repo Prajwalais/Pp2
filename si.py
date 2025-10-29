c2081d83a45c295906e6b2a96238d96d6e20145a
Prajwalais <prajwalbakaningi7@gmail.com>
2025-10-30 01:43:46 +0530

    Create si.py

---
+++ si.py
@@ -0,0 +1,8 @@
+P = 10000  # Principal amount
+R = 5      # Rate of interest per annum
+T = 3      # Time in years
+
+# Calculate Simple Interest
+SI = (P * R * T) / 100
+
+print(f"Simple Interest: ₹{SI}")
